ad648b3a7122556f2723db6c8d8ba8ecda7af228
Kevyn Gandaho <kgandaho@gmail.com>
2019-06-21 15:59:24 -0500

    I'm now able to add Task Types to the database.

--- src/main/java/com/davisassociatescpa/myApp/controllers/TaskTypeController.java
+++ src/main/java/com/davisassociatescpa/myApp/controllers/TaskTypeController.java
@@ -1,13 +1,17 @@
 package com.davisassociatescpa.myApp.controllers;
 
+import com.davisassociatescpa.myApp.models.Client;
 import com.davisassociatescpa.myApp.models.TaskType;
 import com.davisassociatescpa.myApp.models.data.TaskTypeDao;
-import com.davisassociatescpa.myApp.models.data.TasksDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.validation.Valid;
 
 
 @Controller
@@ -20,15 +24,22 @@ public class TaskTypeController {
     @RequestMapping(value = "add", method = RequestMethod.GET)
     public String addTask(Model model) {
         model.addAttribute("title", "Add Task");
-        model.addAttribute("newTaskType", new TaskType());
+        model.addAttribute(new TaskType());
         return "tasks/tasktype.html";
     }
 
     @RequestMapping(value = "add", method = RequestMethod.POST)
-    public String addTask(Model model) {
+    public String processAddTask(@ModelAttribute @Valid TaskType newTaskType,
+                                 Errors errors, Model model) {
         model.addAttribute("title", "Add Task");
-        model.addAttribute("newTaskType", new TaskType());
+        if (errors.hasErrors()) {
+            return "tasks/tasktype.html";
+        }
+
+        taskTypeDao.save(newTaskType);
         return "tasks/tasktype.html";
+
+
     }
 
 }
